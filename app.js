87c7399783c518ea9baab09988695e8db3787ed8
Mayank Srivastav <139525525+mayanksrivastav04@users.noreply.github.com>
2025-09-05 10:06:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -117,6 +117,11 @@ app.delete("/blogs/delete/:id",async (req,res)=>{
 
 })
 
+app.get("/allblogs", async (req, res) => {
+  let blogs = await Blog.find();
+  res.json(blogs); // shows all blogs in JSON
+});
+
 
 
 
